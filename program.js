be262525fc20b57e5b62ec1705b34fb7e9f379dd
David Baca <davidbaca@Davids-MacBook-Pro.local>
2017-08-17 16:30:33 -0700

    Add ability to count results of all die rolls

--- program.js
+++ program.js
@@ -20,22 +20,16 @@ console.log("Rolling the " + mySixDie.type + " die " + mySixDieRolls + " times:"
 for(i=0; i<mySixDieRolls; i++){
     console.log(mySixDie.roll(mySixDie.size));
 }
+console.log("Total count of rolls: " + mySixDie.totalCount);
 
 console.log("Rolling the " + myTwelveDie.type + " die " + myTwelveDieRolls + " times:");
 for(i=0; i<myTwelveDieRolls; i++){
     console.log(myTwelveDie.roll(myTwelveDie.size));
 }
+console.log("Total count of rolls: " + myTwelveDie.totalCount);
 
 console.log("Rolling the " + myTwentyDie.type + " die " + myTwentyDieRolls + " times:");
 for(i=0; i<myTwentyDieRolls; i++){
     console.log(myTwentyDie.roll(myTwentyDie.size));
 }
-
-
-//describe die objects
-console.log("Total rolls of the " + mySixDie.type + " die: " + mySixDie.totalRolls);
-console.log(mySixDie);
-console.log("Total rolls of the " + myTwelveDie.type + " die: " + myTwelveDie.totalRolls);
-console.log(myTwelveDie);
-console.log("Total rolls of the " + myTwentyDie.type + " die: " + myTwentyDie.totalRolls);
-console.log(myTwentyDie);
+console.log("Total count of rolls: " + myTwentyDie.totalCount);
