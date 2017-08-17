9c7118cd6b8c90373a1e3e2dd2e73467ca4685b6
David Baca <davidbaca@Davids-MacBook-Pro.local>
2017-08-17 16:22:32 -0700

    Refactor code to loop through a specific number of die rolls

--- program.js
+++ program.js
@@ -1,3 +1,4 @@
+//using the dice.js file which defines the dice objects
 var dice = require("./dice.js");
 
 //Describes the dice variable we set our die export to
@@ -6,19 +7,30 @@ console.log(dice);
 //Creates new die variable and grabs the exported attributes from the dice.js file
 //which defines the dice objects
 var mySixDie = dice.exp_sixdie;
+var mySixDieRolls = 4;
+
 var myTwelveDie = dice.exp_twelvedie;
+var myTwelveDieRolls = 6;
+
 var myTwentyDie = dice.exp_twentydie;
+var myTwentyDieRolls = 5;
 
-console.log(mySixDie.roll(mySixDie.size));
-console.log(mySixDie.roll(mySixDie.size));
-console.log(mySixDie.roll(mySixDie.size));
 
-console.log(myTwelveDie.roll(myTwelveDie.size));
-console.log(myTwelveDie.roll(myTwelveDie.size));
-console.log(myTwelveDie.roll(myTwelveDie.size));
+console.log("Rolling the " + mySixDie.type + " die " + mySixDieRolls + " times:");
+for(i=0; i<mySixDieRolls; i++){
+    console.log(mySixDie.roll(mySixDie.size));
+}
 
-console.log(myTwentyDie.roll(myTwentyDie.size));
-console.log(myTwentyDie.roll(myTwentyDie.size));
+console.log("Rolling the " + myTwelveDie.type + " die " + myTwelveDieRolls + " times:");
+for(i=0; i<myTwelveDieRolls; i++){
+    console.log(myTwelveDie.roll(myTwelveDie.size));
+}
+
+console.log("Rolling the " + myTwentyDie.type + " die " + myTwentyDieRolls + " times:");
+for(i=0; i<myTwentyDieRolls; i++){
+    console.log(myTwentyDie.roll(myTwentyDie.size));
+}
+
 
 //describe die objects
 console.log("Total rolls of the " + mySixDie.type + " die: " + mySixDie.totalRolls);
